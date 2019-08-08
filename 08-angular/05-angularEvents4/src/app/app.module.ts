96969ca8a497a168404a8a7536d26beb01a3a22a
Adrian Navarro Gabino <adnaga94@gmail.com>
2019-08-08 14:08:25 +0200

    Exercise 04 - Events (Custom pipes)

---
+++ 08-angular/05-angularEvents4/src/app/app.module.ts
@@ -0,0 +1,24 @@
+// Adrián Navarro Gabino
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { EventsShowComponent } from './events-show/events-show.component';
+import { EventFilterPipe } from './pipes/event-filter.pipe';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EventsShowComponent,
+    EventFilterPipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
