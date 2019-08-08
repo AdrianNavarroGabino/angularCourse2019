ed8bcc81435063002bb8666260eb1a8429aa01f6
Adrian Navarro Gabino <adnaga94@gmail.com>
2019-08-08 08:45:38 +0200

    First Angular project

---
+++ 08-angular/01-angularTest/src/app/product-list/product-list.component.ts
@@ -0,0 +1,35 @@
+// Adrián Navarro Gabino
+
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from 'interfaces/i-product';
+
+@Component({
+  selector: 'ulab-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss']
+})
+export class ProductListComponent implements OnInit {
+  title = "Mi lista de productos";
+  headers = {desc: 'Producto', price: 'Precio', avail: 'Disponible'};
+  products: IProduct[] = [{
+    id: 1,
+    desc: 'SSD hard drive',
+    avail: new Date('2016-10-03'),
+    price: 75,
+    imageUrl: 'assets/ssd.jpg',
+    rating: 5
+  },{
+    id: 2,
+    desc: 'LGA1151 Motherboard',
+    avail: new Date('2016-09-15'),
+    price: 96.95,
+    imageUrl: 'assets/motherboard.jpg',
+    rating: 4
+  }];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
