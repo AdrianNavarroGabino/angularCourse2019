1823f43803910b1720e5bfd9dae5228eb6e32b61
Adrian Navarro Gabino <adnaga94@gmail.com>
2019-08-09 11:51:49 +0200

    Exercise 07 - Events (Base64 image)

---
+++ 08-angular/08-angularEvents7/src/app/pipes/event-order-date.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { EventOrderDatePipe } from './event-order-date.pipe';
+
+describe('EventOrderDatePipe', () => {
+  it('create an instance', () => {
+    const pipe = new EventOrderDatePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
