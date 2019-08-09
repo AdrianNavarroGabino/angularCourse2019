eb9f6b79c4ac714a4e23502d6829a793804437ba
Adrian Navarro Gabino <adnaga94@gmail.com>
2019-08-09 11:37:07 +0200

    Exercise 06 - Events (Add event)

---
+++ 08-angular/07-angularEvents6/src/app/events-show/events-show.component.ts
@@ -0,0 +1,77 @@
+// Adrián Navarro Gabino
+
+import { Component, OnInit } from '@angular/core';
+import { IEvent } from 'interfaces/i-event'
+
+@Component({
+  selector: 'app-events-show',
+  templateUrl: './events-show.component.html',
+  styleUrls: ['./events-show.component.scss']
+})
+export class EventsShowComponent implements OnInit {
+  events: IEvent[] = [{
+    title: 'Event 1',
+    image: 'assets/event1.jpg',
+    date: new Date('2019-09-01'),
+    description: 'First event in the Angular course',
+    price: 20
+  },{
+    title: 'Event 2',
+    image: 'assets/event2.jpg',
+    date: new Date('2019-09-30'),
+    description: 'Last event in the Angular course',
+    price: 15.95
+  }]
+  filterSearch: string = '';
+
+  orderByDate() {
+    this.filterSearch = '';
+    this.events.sort(function (a, b) {
+      if(a.date > b.date)
+        return 1;
+      else if(a.date < b.date)
+        return -1;
+      else
+        return 0;
+    })
+  }
+
+  orderByPrice() {
+    this.filterSearch = '';
+    this.events.sort(function (a, b) {
+      if(a.price > b.price)
+        return 1;
+      else if(a.price < b.price)
+        return -1;
+      else
+        return 0;
+    })
+  }
+
+  newEvent: IEvent = {
+    title: '',
+    description: '',
+    image: '',
+    price: 0,
+    date: null
+  };
+
+  addEvent() {
+    this.events.push(this.newEvent);
+    this.newEvent = {
+      title: '',
+      description: '',
+      image: '',
+      price: 0,
+      date: null
+    };
+  }
+
+  changeImage() {}
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
