9dc5db0839e37f196c4084473d2e3821a22cd69c
Adrian Navarro Gabino <adnaga94@gmail.com>
2019-08-09 11:35:45 +0200

    Order by price

---
+++ 08-angular/01-angularTest/src/app/pipes/product-order.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { IProduct } from 'interfaces/i-product';
+
+@Pipe({
+  name: 'productOrder'
+})
+export class ProductOrderPipe implements PipeTransform {
+
+  transform(products: IProduct[]): IProduct[] {
+    return products.sort(function (a: IProduct, b: IProduct) {
+      if(a.price > b.price)
+        return 1;
+      else if(a.price < b.price)
+        return -1
+      else
+        return 0;
+    })
+  }
+
+}
